ddb8ec9ffd94f9c1c40a1e38618401e0c9f0e077
Perberos <perberos@gmail.com>
2011-11-14 18:46:55 -0300

    updating autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -4,19 +4,22 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME=mate-system-monitor
+PKG_NAME="mate-system-monitor"
 
-(test -f $srcdir/configure.in \
-  && test -f $srcdir/autogen.sh \
-  && test -d $srcdir/src) || {
+(test -f $srcdir/configure.in) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
 }
 
 which mate-autogen.sh || {
-    echo "You need to install mate-common from the MATE CVS"
+    echo "You need to install mate-common from the MATE Git"
     exit 1
 }
 
-REQUIRED_AUTOMAKE_VERSION=1.9 MATE_DATADIR="$mate_datadir" USE_COMMON_DOC_BUILD=yes . mate-autogen.sh
+REQUIRED_AUTOMAKE_VERSION=1.9
+MATE_DATADIR="$mate_datadir"
+USE_COMMON_DOC_BUILD=yes
+
+. mate-autogen.sh
+

bc87b7b4a1b199606a29877067ae657f850f1618
infirit <infirit@gmail.com>
2014-12-16 11:59:03 +0100

    g_type_init is deprecated in Glib 2.36

--- src/procman.cpp
+++ src/procman.cpp
@@ -712,7 +712,9 @@ main (int argc, char *argv[])
 		exit (0);
 	}
 
+#if !GLIB_CHECK_VERSION (2, 36, 0)
 	g_type_init ();
+#endif
 
 	gtk_window_set_default_icon_name ("utilities-system-monitor");
 	g_set_application_name(_("System Monitor"));

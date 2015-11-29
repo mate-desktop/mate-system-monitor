7ee20e415d503c63a88858ae1e8c85c737774abf
Wolfgang Ulbrich <chat-to-me@raveit.de>
2015-11-29 22:03:16 +0100

    GTK3: don't use deprecated gtk_{v/h}-box

--- src/sysinfo.cpp
+++ src/sysinfo.cpp
@@ -31,6 +31,10 @@
 #include "procman.h"
 #include "util.h"
 
+#if GTK_CHECK_VERSION (3, 0, 0)
+#define gtk_hbox_new(X,Y) gtk_box_new(GTK_ORIENTATION_HORIZONTAL,Y)
+#define gtk_vbox_new(X,Y) gtk_box_new(GTK_ORIENTATION_VERTICAL,Y)
+#endif
 
 using std::string;
 using std::vector;

mate-desktop/mate-system-monitor

commit 9ae0be5ccf7df2f35c13653da97771b06cf6603f

Fix a few of mem leaks. [Backported from old gnome-system-monitor]

src/util.cpp
@@ -495,13 +495,19 @@ namespace procman
 
   std::string format_network(guint64 rate, guint64 max_rate)
   {
-    return procman::format_size(rate, max_rate, ProcData::get_instance()->config.network_in_bits);
+    char* bytes = procman::format_size(rate, max_rate, ProcData::get_instance()->config.network_in_bits);
+    std::string formatted(bytes);
+    g_free(bytes);
+    return formatted;
   }
 
 
   std::string format_network_rate(guint64 rate, guint64 max_rate)
   {
-    return procman::format_rate(rate, max_rate, ProcData::get_instance()->config.network_in_bits);
+    char* bytes = procman::format_size(rate, max_rate, ProcData::get_instance()->config.network_in_bits);
+    std::string formatted(bytes);
+    g_free(bytes);
+    return formatted;
   }
 
 }

src/procman.cpp
@@ -189,7 +189,7 @@ static void
 color_changed_cb (GSettings *settings, const gchar *key, gpointer data)
 {
 	ProcData * const procdata = static_cast<ProcData*>(data);
-	const gchar *color = g_settings_get_string (settings, key); 
+	gchar *color = g_settings_get_string (settings, key);
 
 	if (g_str_has_prefix (key, "cpu-color")) {
 		for (int i = 0; i < procdata->config.num_cpus; i++) {
@@ -220,6 +220,7 @@ color_changed_cb (GSettings *settings, const gchar *key, gpointer data)
 	else {
 		g_assert_not_reached();
 	}
+	g_free (color);
 }
 
 
@@ -450,6 +451,7 @@ procman_get_tree_state (GSettings *settings, GtkWidget *tree, const gchar *child
 
 		proctable_set_columns_order(GTK_TREE_VIEW(tree), order);
 
+		g_variant_unref(value);
 		g_slist_free(order);
 	}
 

commit 8110ba34b9b447353e1e344ea8fa92c30fe9bd78

fix copy-paste error, use proper type, keep it simple
- wrong function name introduced in https://github.com/mate-desktop/mate-system-monitor/commit/9ae0be5ccf7df2f35c13653da97771b06cf6603f
- format_rate function already returns a string, no need to convert

src/util.cpp
@@ -601,10 +601,7 @@ namespace procman
 
     std::string format_network_rate(guint64 rate, guint64 max_rate)
     {
-        char* bytes = procman::format_size(rate, max_rate, ProcData::get_instance()->config.network_in_bits);
-        std::string formatted(bytes);
-        g_free(bytes);
-        return formatted;
+        return procman::format_rate(rate, max_rate, ProcData::get_instance()->config.network_in_bits);
     }
 
 }
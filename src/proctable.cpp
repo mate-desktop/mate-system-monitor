b697b585d3e492014682f4d176b0dd2bbb05bef7
monsta <monsta@inbox.ru>
2016-02-29 17:44:52 +0300

    process table: avoid expanding collapsed nodes on every refresh
    from
    https://git.gnome.org/browse/gnome-system-monitor/commit/?id=f7f4c5506bc47f7cd340ce02404029a0ca6185e0

--- src/proctable.cpp
+++ src/proctable.cpp
@@ -1025,7 +1025,6 @@ proctable_update_list (ProcData * const procdata)
             if (procdata->config.show_tree)
             {
                 selection.save(procdata->tree);
-                proctable_clear_tree(procdata);
             }
             break;
 
@@ -1035,7 +1034,6 @@ proctable_update_list (ProcData * const procdata)
             if (procdata->config.show_tree)
             {
                 selection.save(procdata->tree);
-                proctable_clear_tree(procdata);
             }
             break;
     }

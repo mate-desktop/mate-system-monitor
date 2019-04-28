7a2cbcc6abbe5e50aea4384843838d7f211dfbb6
rbuj <robert.buj@gmail.com>
2019-04-28 18:59:01 +0200

    msm_execute_helper: fix memory leak

--- tools/msm_execute_helper.c
+++ tools/msm_execute_helper.c
@@ -8,10 +8,12 @@ int main(int argc, char* argv[])
     gchar **argv_modified = g_new0 (gchar *, argc + 1);
     memcpy (argv_modified, argv, argc * sizeof (char*));
     argv_modified[0] = COMMAND;
+    int errsv = 0;
 
     if (execvp (COMMAND, argv_modified) == -1) {
-        return errno;
+        errsv = errno;
     }
 
-    return 0;
+    g_free (argv_modified);
+    return errsv;
 }

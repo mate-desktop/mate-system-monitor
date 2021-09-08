ddeb66b5af61a5367c8b72a5e5845317e82b62a4
mbkma <johannes.unruh@fau.de>
2021-09-08 21:28:55 +0200

    Fix Information Fields not being saved

--- src/procman.cpp
+++ src/procman.cpp
@@ -97,10 +97,6 @@ procman_get_tree_state (GSettings *settings, GtkWidget *tree, const gchar *child
             visible = g_settings_get_boolean (pt_settings, key);
             g_free (key);
 
-            column = gtk_tree_view_get_column (GTK_TREE_VIEW (tree), id);
-            if (column == NULL)
-                continue;
-
             gtk_tree_view_column_set_visible (column, visible);
             if (visible) {
                 /* ensure column is really visible */
